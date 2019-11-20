c75fe23c535ad13dd4878e01d314e3b9679787ce
ingjae <hij1995@naver.com>
2019-11-21 01:03:54 +0900

    .

--- src/people_tracker.py
+++ src/people_tracker.py
@@ -88,9 +88,7 @@ if __name__ == "__main__":
         rasimage_msg.encoding="rgb8"
         pub.publish(rasimage_msg)
         
-        if ret == False:
-            print("Error getting image")
-            continue
+        
 
         # # Convert to greyscale for detection
         # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -120,35 +118,35 @@ if __name__ == "__main__":
         #     # Get the center of the face
         #     x = x + (w/2)
         #     y = y + (h/2)
-            x = center_position.x_mid * FRAME_W
-            y = center_position.x_mid * FRAME_H 
+        x = center_position.x_mid * FRAME_W
+        y = center_position.x_mid * FRAME_H 
 
-            # Correct relative to center of image
-            turn_x  = float(x - (FRAME_W/2))
-            turn_y  = float(y - (FRAME_H/2))
+        # Correct relative to center of image
+        turn_x  = float(x - (FRAME_W/2))
+        turn_y  = float(y - (FRAME_H/2))
 
-            # Convert to percentage offset
-            turn_x  /= float(FRAME_W/2)
-            turn_y  /= float(FRAME_H/2)
+        # Convert to percentage offset
+        turn_x  /= float(FRAME_W/2)
+        turn_y  /= float(FRAME_H/2)
 
-            # Scale offset to degrees
-            turn_x   *= 2.5 # VFOV
-            turn_y   *= 2.5 # HFOV
-            cam_pan  += -turn_x
-            cam_tilt += turn_y
+        # Scale offset to degrees
+        turn_x   *= 2.5 # VFOV
+        turn_y   *= 2.5 # HFOV
+        cam_pan  += -turn_x
+        cam_tilt += turn_y
 
-            #print(cam_pan-90, cam_tilt-90)
-            print(x,y)
+        #print(cam_pan-90, cam_tilt-90)
+        print(x,y)
 
-            # Clamp Pan/Tilt to 0 to 180 degrees
-            cam_pan = max(0,min(180,cam_pan))
-            cam_tilt = max(0,min(180,cam_tilt))
+        # Clamp Pan/Tilt to 0 to 180 degrees
+        cam_pan = max(0,min(180,cam_pan))
+        cam_tilt = max(0,min(180,cam_tilt))
 
-            # Update the servos
-            pan(int(cam_pan-90))
-            tilt(int(cam_tilt-90))
+        # Update the servos
+        pan(int(cam_pan-90))
+        tilt(int(cam_tilt-90))
 
-            break
+        break
 ################################################
 ################################################
 
