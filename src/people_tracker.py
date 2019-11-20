1feb5346894a527d482444ef7df9802b1b17f9ef
ingjae <hij1995@naver.com>
2019-11-21 00:22:14 +0900

    .

--- src/people_tracker.py
+++ src/people_tracker.py
@@ -83,6 +83,7 @@ if __name__ == "__main__":
         # This line lets you mount the camera the "right" way up, with neopixels above
         rasimage = frame
         rasimage_msg = bridge.cv2_to_imgmsg(rasimage, encoding="passthrough")
+        rasimage_msg.encoding="bgr8"
         pub.publish(rasimage_msg)
         frame = cv2.flip(frame, -1)
         
