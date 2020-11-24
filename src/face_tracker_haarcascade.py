fb15bdf91a4ee5a4e307593615a99d0b7f359780
kissw <kiminstein@naver.com>
2020-11-24 13:03:26 +0900

    .

--- src/face_tracker_haarcascade.py
+++ src/face_tracker_haarcascade.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         gray = cv2.flip(gray, -1)
         gray = cv2.flip(gray, 1)
-        faces = face_cascade.detectMultiScale(gray, 1.5, 5)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
 
 
