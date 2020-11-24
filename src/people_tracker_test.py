fbf237983b3d0c77971bfb0f5ecfe51cacaf013d
kissw <kiminstein@naver.com>
2020-11-24 12:06:04 +0900

    .

--- src/people_tracker_test.py
+++ src/people_tracker_test.py
@@ -31,7 +31,7 @@ tilt(cam_tilt-90)
 light_mode(WS2812)
 
 if __name__ == "__main__":
-    rospy.init_node('sub_center_position', anonymous=False)
+    rospy.init_node('pantilt_node', anonymous=False)
 
     while not rospy.is_shutdown():
         ret, img = cap.read()
@@ -43,14 +43,19 @@ if __name__ == "__main__":
 
 
         for (x,y,w,h) in faces:
-            cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+            img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
             roi_gray = gray[y:y+h, x:x+w]
             roi_color = img[y:y+h, x:x+w]
-
+            
         rasimage = img
         rasimage_msg = bridge.cv2_to_imgmsg(rasimage, encoding="passthrough")
         rasimage_msg.encoding="rgb8"
         rasimage_pub.publish(rasimage_msg)
+
+        # cv2.imshow('img',img)
+        # k = cv2.waitKey(30) & 0xff
+        # if k == 27:
+        #     break
 
     cap.release()
     # cv2.destroyAllWindows()
